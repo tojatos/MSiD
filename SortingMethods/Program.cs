36fa78767dc69e7cf468006174d91e40b73b0c6f
tojatos <tojatos@gmail.com>
2020-03-23 00:04:49 +0100

    Comparision of sorting algorithms implemented

--- SortingMethods/Program.cs
+++ SortingMethods/Program.cs
@@ -1,27 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 namespace SortingMethods
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly List<ISortingAlgorithm> SortingAlgorithms = new List<ISortingAlgorithm> {
+            new Quicksort(),
+            new ShellSort(ShellSortSeries.Shell),
+            new ShellSort(ShellSortSeries.FrankLazarus),
+            new MergeSort(),
+        };
+
+        private static readonly List<List<float>> ToSort = new List<List<float>>
         {
-            float[] numbersToSort1 = { 1.6f, 6.5f, 1.5f, 1.55f, 1.51f, -6, 3, 5, 21 };
-            int[] numbersToSort2 = { 1021, 6, 5, 5, 0, 0, 3};
+            new List<float>{ 1.6f, 6.5f, 1.5f, 1.55f, 1.51f, -6, 3, 5, 21 },
+            new List<float>{ 1021, 6, 5, 5, 0, 0, 3 },
+        };
 
-            ISortingAlgorithm quicksort = new Quicksort();
-            ISortingAlgorithm mergesort = new MergeSort();
+        private static void Main()
+        {
+            StartFullSimulation();
+        }
 
-            Console.WriteLine($"Numbers before sorting:");
-            Console.WriteLine(string.Join(' ', numbersToSort1));
-            Console.WriteLine(string.Join(' ', numbersToSort2));
+        private static void StartFullSimulation()
+        {
+            foreach (ISortingAlgorithm algorithm in SortingAlgorithms)
+            {
+                foreach (List<float> numbersToSortReadonly in ToSort)
+                {
+                    float[] numbersToSort = numbersToSortReadonly.ToArray();
+                    Console.WriteLine($"Simulating {algorithm.Name}");
+                    Console.WriteLine($"Numbers before sorting: {string.Join(' ', numbersToSort)}");
+                    double timeInSeconds = TimeMeasurer.Measure(() => algorithm.Sort(ref numbersToSort)).TotalSeconds;
+                    Console.WriteLine($"Numbers after sorting: {string.Join(' ', numbersToSort)}");
+                    Console.WriteLine($"Time taken: {timeInSeconds}");
+                }
+            }
 
-            quicksort.Sort(ref numbersToSort1);
-            mergesort.Sort(ref numbersToSort2);
-
-            Console.WriteLine($"Numbers after sorting:");
-            Console.WriteLine(string.Join(' ', numbersToSort1));
-            Console.WriteLine(string.Join(' ', numbersToSort2));
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
