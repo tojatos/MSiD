2e3422bb41514f8d7c66da17dfa057e6c19ad245
tojatos <tojatos@gmail.com>
2020-03-23 00:02:18 +0100

    TimeMeasurer added

---
+++ SortingMethods/TimeMeasurer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SortingMethods
+{
+    public static class TimeMeasurer
+    {
+        private static readonly Stopwatch Sw = new Stopwatch();
+
+        public static TimeSpan Measure(Action a)
+        {
+            Sw.Reset();
+            Sw.Start();
+            a();
+            Sw.Stop();
+
+            return Sw.Elapsed;
+        }
+    }
+}
