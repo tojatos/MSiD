3c0e59e643f3183124fb2d90688c52db1608c832
tojatos <tojatos@gmail.com>
2020-04-26 19:55:39 +0200

    Implemented extraction of bid/ask from apis

--- finance.py
+++ finance.py
@@ -3,39 +3,79 @@
 import urllib.request, json
 from time import sleep
 
-markets=[
-    'BTC-LTC',
-    'BTC-DOGE',
-    'BTC-PPC',
-    'BTC-FTC',
-    'BTC-RDD',
+market_names = [
+    'BTC',
+    'LTC',
+    'ETH',
+    'XRP',
 ]
+
+apis = [
+    'bittrex',
+    'bitbay',
+    'kraken',
+    'binance',
+]
+
+market_mappings = {
+    'bittrex': [
+        'USD-BTC',
+        'USD-LTC',
+        'USD-ETH',
+        'USD-XRP',
+    ],
+    'bitbay': [
+        'BTC-USD',
+        'LTC-USD',
+        'ETH-USD',
+        'XRP-USD',
+    ],
+    'kraken': [
+        'XBTUSD',
+        'LTCUSD',
+        'ETHUSD',
+        'XRPUSD',
+    ],
+    'binance': [
+        'BTCTUSD',
+        'LTCTUSD',
+        'ETHTUSD',
+        'XRPTUSD',
+    ],
+}
+
+urls = {
+    'bittrex': 'https://api.bittrex.com/api/v1.1/public/getticker?market=',
+    'bitbay':  'https://api.bitbay.net/rest/trading/ticker/',
+    'kraken':  'https://api.kraken.com/0/public/Ticker?pair=',
+    'binance': 'https://api.binance.com/api/v3/ticker/bookTicker?symbol=',
+}
 
 def get_json_data(url):
     with urllib.request.urlopen(url) as u:
         return json.loads(u.read().decode())
 
-def get_buy_sell(market):
-    url = f"https://api.bittrex.com/api/v1.1/public/getticker?market={market}"
+def extract_buy_sell(api, data):
+    if api == 'bittrex':
+        return (data['result']['Bid'], data['result']['Ask'])
+    if api == 'bitbay':
+        return (float(data['ticker']['highestBid']), float(data['ticker']['lowestAsk']))
+    if api == 'kraken':
+        values = next(iter(data['result'].values()))
+        return (float(values["b"][0]), float(values["a"][0]))
+    if api == 'binance':
+        return (float(data['bidPrice']), float(data['askPrice']))
+
+def get_buy_sell(api, market):
+    url = f"{urls[api]}{market}"
     data = get_json_data(url)
-    return (data['result']['Bid'], data['result']['Ask'])
+    return extract_buy_sell(api, data)
 
-def print_buy_sell():
-    for market in markets:
-        (buy, sell) = get_buy_sell(market)
-        print(f'{market}: Buy for {buy}, Sell for {sell}')
+for api in apis:
+    print(api)
+    print('-=-=-=-=-=-=-=-=-=-=-')
+    for i, market in enumerate(market_mappings[api]):
+        print(f'{market_names[i]}: {get_buy_sell(api, market)}')
+    print('-=-=-=-=-=-=-=-=-=-=-')
+    print()
 
-def get_percent_diff(buy, sell):
-    return 1 - (sell - buy) / buy
-
-def print_buy_sell_percent_diff():
-    data = {market:get_percent_diff(*get_buy_sell(market)) for market in markets}
-    print('\n'.join([f'{market}: {percent}' for (market, percent) in data.items()]))
-
-def monitor_buy_sell_percent_diff():
-    while True:
-        print_buy_sell_percent_diff()
-        sleep(5)
-
-# print_buy_sell()
-monitor_buy_sell_percent_diff()
