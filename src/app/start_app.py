0b25d373acf6791198aacf592b7d7c3f599063bc
tojatos <tojatos@gmail.com>
2020-05-16 16:11:30 +0200

    Wallet update and styles added

--- src/app/start_app.py
+++ src/app/start_app.py
@@ -9,7 +9,7 @@ import json
 from flask import Flask, render_template, Response, request, redirect, url_for
 from app.data_gatherer import gather_data
 import app.data as data
-from app.config import INITIAL_WALLET
+from app.config import INITIAL_WALLET, CURRENCIES
 from threading import Thread
 
 app = Flask(__name__)
@@ -19,18 +19,26 @@ def main():
     if wallet_empty():
         page = render_template('wallet_init.html', initial_wallet=INITIAL_WALLET)
     else:
-        print(data.get_wallet())
         page = render_template('main_view.html', wallet=data.get_wallet())
     return page
 
 @app.route("/init_wallet", methods=['POST'])
 def init_wallet():
-    currencies = INITIAL_WALLET.keys()
     curr_val_map = {}
-    for currency in currencies:
-        curr_val_map[currency] = request.form.get(currency)
+    for currency in CURRENCIES:
+        curr_val_map[currency] = float(request.form.get(currency))
     data.insert_into_wallet(curr_val_map)
     return redirect(url_for('main'))
+
+@app.route("/update_wallet", methods=['POST'])
+def update_wallet():
+    for currency in CURRENCIES:
+        data.update_in_wallet(currency, float(request.form.get(currency)))
+    return redirect(url_for('main'))
+
+@app.route("/get_wallet")
+def get_wallet():
+    return Response(json.dumps(data.get_wallet()), mimetype='application/json')
 
 def wallet_empty():
     return not bool(data.get_wallet())
